8621fc88fea24d65ad48f754aed28493d68e3a38
Micah Shallom <micahshallom@gmail.com>
2022-11-28 00:41:54 +0100

    added

--- output.tf
+++ output.tf
@@ -1,3 +1,33 @@
 output "KUBERNETES_PUBLIC_ADDRESS" {
     value = aws_lb.lb.dns_name
 }
+
+output "VPC_ID" {
+    value = aws_vpc.vpc.id
+}
+output "SUBNET_ID" {
+    value = aws_subnet.subnet.id
+}
+
+output "INTERNET_GATEWAY_ID" {
+    value = aws_internet_gateway.igw.id
+}
+output "ROUTE_TABLE_ID" {
+    value = aws_route_table.rtb.id
+}
+
+output "SECURITY_GROUP_ID" {
+    value = aws_security_group.cluster-sg.id
+}
+
+output "LOAD_BALANCER_ARN" {
+    value = aws_lb.lb.arn
+}
+
+output "TARGET_GROUP_ARN" {
+    value = aws_lb_target_group.lb_tg.arn
+}
+
+output "IMAGE_ID" {
+    value = data.aws_ami.latest-ubuntu-linux-image.id
+}
---
+++ script.sh
@@ -0,0 +1,9 @@
+export IMAGE_ID = terraform output IMAGE_ID
+export INTERNET_GATEWAY_ID = terraform output INTERNET_GATEWAY_ID
+export KUBERNETES_PUBLIC_ADDRESS = terraform output KUBERNETES_PUBLIC_ADDRESS
+export LOAD_BALANCER_ARN = terraform output LOAD_BALANCER_ARN
+export ROUTE_TABLE_ID = terraform output ROUTE_TABLE_ID
+export SECURITY_GROUP_ID = terraform output SECURITY_GROUP_ID
+export SUBNET_ID = terraform output SUBNET_ID
+export TARGET_GROUP_ARN = terraform output TARGET_GROUP_ARN
+export VPC_ID = terraform output VPC_ID
